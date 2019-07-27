bb4fef6f4f8fe06365906ebeae917330659fd69a
Morgan-Mischo <goramorgan@gmail.com>
2019-07-27 16:41:20 -0600

    initial work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './components/login/login'; 
+import Login from './components/login/Login'; 
+import flower from './components/pictues/flower.png'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Login/>
+    <img src={flower} className="flower" alt="flower" />
     </div>
   );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,1 +1,17 @@
-import './login'
+import './login.css'
+import React, { Component }  from 'react'; 
+
+class Login extends Component {
+
+    render () {
+        return (
+            <div className="login-container">
+                <h3 className="login">Login or Register</h3>
+                <button className="login">Login</button>
+                <button className="Register">Register</button>
+            </div>
+        )
+    }
+}
+
+export default Login; 
